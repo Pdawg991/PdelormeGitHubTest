d1158aa84af584f5f9bf55c19cc5df0abd30aaa9
Pdawg991 <106412178+Pdawg991@users.noreply.github.com>
2023-03-14 22:00:46 -0400

    index changes

--- api/quotes/index.php
+++ api/quotes/index.php
@@ -7,3 +7,17 @@
         header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
         exit();
     } 
+/?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<h1>MY REST API</h1>
+<body>
+    
+</body>
+</html>
