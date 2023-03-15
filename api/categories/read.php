3a0f378c7a96bad6a295f33b9ab71127e0816b8b
Pdawg991 <106412178+Pdawg991@users.noreply.github.com>
2023-03-15 00:26:09 -0400

    cat file

--- api/categories/read.php
+++ api/categories/read.php
@@ -21,7 +21,7 @@ $categories_arr['data'] = array();
 while($row = $result->fetch(PDO::FETCH_ASSOC)){
 extract($row);
 
-$category_item = array('category' => $category);
+$category_item = array('id' => $id,'category' => $category);
 
 array_push($categories_arr['data'], $category_item);
 
