02841a916669c4e53a85a7970f5613afbca1f7ef
Pdawg991 <106412178+Pdawg991@users.noreply.github.com>
2023-03-21 15:57:57 -0400

    bugs

--- api/categories/update.php
+++ api/categories/update.php
@@ -4,39 +4,23 @@ header('Content-Type: application/json');
 header('Access-Control-Allow-Methods: PUT');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
-// Include necessary files
 include_once '../../config/database.php';
 include_once '../../models/Category.php';
 
-// Create database object and connect to database
 $database = new Database();
 $db = $database->connect();
 
-// Create category object
-$category = new Category($db);
+$post = new Category($db);
 
-// Read all categories
-$result = $category->read();
-
-// Get number of rows returned
-$num = $result->rowCount();
-
-// If there are categories, create an array and loop through them
-if ($num > 0){
-    $categories_arr = array();
-    $categories_arr['data'] = array();
-
-    while($row = $result->fetch(PDO::FETCH_ASSOC)){
-        extract($row);
-
-        // Create category item and add to array
-        $category_item = array('id' => $id,'category' => $category);
-        array_push($categories_arr['data'], $category_item);
-    }
-    // Output categories array as JSON
-    echo json_encode($categories_arr['data']);
+$data = json_decode(file_get_contents("php://input"));
+//Set ID to update
+if(!isset($data->id) || !isset($data->category)){
+    echo json_encode(array('message' => 'Missing Required Parameters'));
 }
-// If there are no categories, output an error message as JSON
 else{
-    echo json_encode(array('message'=> 'Missing Required Parameters'));
+    $post->id = $data->id;
+    $post->category = $data->category;
+if ($post->update()){
+    echo json_encode(array('id'=> $post->id,  'category' => $post->category));
+}
 }
